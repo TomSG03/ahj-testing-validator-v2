38cb24c0e934e19dd7ae8e4278dd5c678ce7b95b
Tomilov Sergey <tomexpert@yandex.ru>
2021-11-23 12:23:01 +0800

    test

--- src/js/__test__/cells.test.js
+++ src/js/__test__/cells.test.js
@@ -1,3 +1,9 @@
+import Cells from '../cells';
+
 test('Created Cells class', () => {
-  expect('A').toEqual('A');
+  const field = document.createElement('div');
+
+  const num = new Cells(4, 4, field);
+
+  expect(num.x).toBe(4);
 });
